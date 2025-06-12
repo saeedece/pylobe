93ca9db7c80b3cfa3ff01f33b779a98afcada25c
saeed <git@saeedc.com>
2025-06-12 17:40:39 -0400

    Test: Switch default gain pattern from linear to planar.
    It's better to have an assymetric pattern for testing reasons, especially when tsting the interpolation functions for correctness.

--- test/conftest.py
+++ test/conftest.py
@@ -2,12 +2,11 @@ import numpy as np
 import numpy.typing as npt
 import pytest
 
-from pylobe.pattern import linear_array_factor
+from pylobe.pattern import planar_array_factor
 from pylobe.transform import (
     extract_phi_plane,
     extract_theta_cone,
     linear_to_db,
-    unit_normalize_db_pattern,
 )
 from pylobe.utils import build_equiangular_grid
 
@@ -19,11 +18,18 @@ def simple_grid():
 
 @pytest.fixture()
 def factor():
-    return linear_array_factor(
-        theta_grid=build_equiangular_grid()[1],
-        kd=np.complex64(1.0),
-        beta=np.zeros(shape=(1,), dtype=np.complex64),
-        num_elements=5,
+    phi_grid, theta_grid = build_equiangular_grid()
+    return planar_array_factor(
+        theta_grid,
+        phi_grid,
+        m=5,
+        kdx=np.complex64(5 * np.pi / 7),
+        betax=np.zeros(shape=(1,), dtype=np.complex64),
+        im1=np.ones(shape=(5,), dtype=np.complex64),
+        n=3,
+        kdy=np.complex64(np.pi / 2),
+        betay=np.zeros(shape=(1,), dtype=np.complex64),
+        i1n=np.ones(shape=(3,), dtype=np.complex64),
     )
 
 
@@ -50,24 +56,3 @@ def slice_xz(intensity: npt.NDArray[np.floating]):
 @pytest.fixture()
 def slice_yz(intensity: npt.NDArray[np.floating]):
     return extract_phi_plane(intensity, np.pi / 2, endpoint=False)
-
-
-@pytest.fixture()
-def intensity_norm(intensity):
-    return intensity - np.max(intensity)
-    # return unit_normalize_db_pattern(intensity)
-
-
-@pytest.fixture()
-def slice_xy_norm(intensity_norm: npt.NDArray[np.floating]):
-    return extract_theta_cone(intensity_norm, np.pi / 2, endpoint=False)
-
-
-@pytest.fixture()
-def slice_xz_norm(intensity_norm: npt.NDArray[np.floating]):
-    return extract_phi_plane(intensity_norm, 0, endpoint=False)
-
-
-@pytest.fixture()
-def slice_yz_norm(intensity_norm: npt.NDArray[np.floating]):
-    return extract_phi_plane(intensity_norm, np.pi / 2, endpoint=False)
